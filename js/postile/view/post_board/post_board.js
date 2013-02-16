9af469d8a94bb3a71578a6d29f2a5b500e9c8c29
guanlun <guanlunzhao@gmail.com>
2013-02-16 15:47:22 +0800

    search implemented

--- js/postile/view/post_board/post_board.js
+++ js/postile/view/post_board/post_board.js
@@ -220,6 +220,99 @@ postile.view.post_board.handlers.keypress = function(instance, e){
     }    
 }
 
+/* guanlun hacks */
+postile.view.post_board.handlers.search = function(instance) {
+    // people search result
+    var search_result_people = goog.dom.getElement("search_result_people");
+    var people_list = goog.dom.getElementByClass("search_result_content_list", search_result_people);
+    people_list.innerHTML = "";
+
+    // topic search result
+    var search_result_topic = goog.dom.getElement("search_result_topic");
+    var topic_list = goog.dom.getElementByClass("search_result_content_list", search_result_topic);
+    topic_list.innerHTML = "";
+
+    // post search result
+    var search_result_post = goog.dom.getElement("search_result_post");
+    var post_list = goog.dom.getElementByClass("search_result_content_list", search_result_post);
+    post_list.innerHTML = "";
+
+    var search_value = goog.dom.getElement("search_input_field").value;
+
+    postile.ajax(['search','search_user'], { search: search_value }, function(data) {
+        user_arr = JSON.parse(data.message);
+        for (i in user_arr) {
+            user = user_arr[i];
+
+            // people item
+            var people_result = goog.dom.createDom("div", "search_result_item search_result_people");
+            goog.dom.appendChild(people_list, people_result);
+
+            // profile image
+            var result_image = goog.dom.createDom("div", "search_result_image");
+            goog.dom.appendChild(people_result, result_image);
+
+            // right container
+            var result_right_container = goog.dom.createDom("div", "search_result_right_container");
+            goog.dom.appendChild(people_result, result_right_container);
+
+            // title (username)
+            var result_item_title = goog.dom.createDom("div", "search_result_item_title");
+            result_item_title.innerHTML = user.username;
+            goog.dom.appendChild(result_right_container, result_item_title);
+
+            // info (email)
+            var result_item_info = goog.dom.createDom("div", "search_result_item_info");
+            result_item_info.innerHTML = user.email;
+            goog.dom.appendChild(result_right_container, result_item_info);
+        }
+    });
+
+    postile.ajax(['search','search_topic'], { search: search_value }, function(data) {
+        topic_arr = JSON.parse(data.message);
+        for (i in topic_arr) {
+            topic = topic_arr[i];
+            console.log(topic);
+
+            // topic item
+            var topic_result = goog.dom.createDom("div", "search_result_item search_result_topic");
+            goog.dom.appendChild(topic_list, topic_result);
+
+            // profile image
+            var result_image = goog.dom.createDom("div", "search_result_image");
+            goog.dom.appendChild(topic_result, result_image);
+
+            // right container
+            var result_right_container = goog.dom.createDom("div", "search_result_right_container");
+            goog.dom.appendChild(topic_result, result_right_container);
+
+            // title (name)
+            var result_item_title = goog.dom.createDom("div", "search_result_item_title");
+            result_item_title.innerHTML = topic.name;
+            goog.dom.appendChild(result_right_container, result_item_title);
+
+            // info (description)
+            var result_item_info = goog.dom.createDom("div", "search_result_item_info");
+            result_item_info.innerHTML = topic.description;
+            goog.dom.appendChild(result_right_container, result_item_info);
+        }
+    });
+
+    postile.ajax(['search','search_post'], { search: search_value }, function(data) {
+        post_arr = JSON.parse(data.message); // not sure why here I need one more level of parsing
+        for (i in post_arr) {
+            post = post_arr[i];
+
+            var post_result = goog.dom.createDom("div", "search_result_item search_result_post");
+            goog.dom.appendChild(post_list, post_result);
+
+            var item_title = goog.dom.createDom("div", "search_result_item_title");
+            item_title.innerHTML = post.title;
+            goog.dom.appendChild(post_result, item_title);
+        }
+    });
+}
+
 postile.view.post_board.PostBoard = function(topic_id) { //constructor
     var i;
     var keyHandler;
@@ -253,6 +346,9 @@ postile.view.post_board.PostBoard = function(topic_id) { //constructor
     /* guanlun hacking */
     this.wrapper = goog.dom.getElement("wrapper");
     goog.dom.appendChild(this.wrapper, this.viewport);
+
+    this.search_input_field = goog.dom.getElement("search_input_field");
+    goog.events.listen(this.search_input_field, goog.events.EventType.CHANGE, postile.view.post_board.handlers.search);
     /* guanlun hacked */
 
     // goog.dom.appendChild(postile.wrapper, this.viewport);
