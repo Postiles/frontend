ce43cad2848fc8d9db3d19241d539e1de67ff2ad
FeiAline <yufei.flyingfish@gmail.com>
2013-03-13 11:09:17 +0800

    Confirm Button Tested
    但是现在removefromBoard这个function似乎没有用，只能再目前版面消失，如果移动或者reflesh 又会回来。

--- js/postile/view/confirm_delete.js
+++ js/postile/view/confirm_delete.js
@@ -6,16 +6,28 @@ goog.require('postile.view');
 goog.require('goog.dom');
 goog.require('postile.fx.effects');
 
-postile.view.confirm_delete.ConfirmDelete = function() {
+postile.view.confirm_delete.ConfirmDelete = function(input_instance) {
+	var instance = input_instance;
+
 	postile.view.TipView.call(this);
 	postile.ui.load(this.container, postile.staticResource(['confirm_delete.html']));
 	this.container.className = 'confirm_delete';
-	console.log("confirm delete loaded");
+	this.container.style.top = '-2px';
+	this.container.style.left = '22px';
 
-    goog.dom.appendChild(document.body, this.container);
+	var self = this;
+	
+	var bt_delete = this.container.lastChild.previousSibling.lastChild.previousSibling;
+	var bt_cancel = bt_delete.previousSibling.previousSibling;
+	
+	goog.events.listen(bt_delete, goog.events.EventType.CLICK, function(){
+        instance.removeFromBoard();
+    });
+    goog.events.listen(bt_cancel, goog.events.EventType.CLICK, function(){
+        self.close();
+    });
+
 }
 
 goog.inherits(postile.view.confirm_delete.ConfirmDelete, postile.view.TipView);
 postile.view.confirm_delete.ConfirmDelete.prototype.unloaded_stylesheets = ['confirm_delete.css'];
-
-console.log();
