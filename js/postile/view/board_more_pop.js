054be6fc86a7e793a40a3b13919ed8bd7dfbc359
FeiAline <yufei.flyingfish@gmail.com>
2013-03-14 23:52:36 +0800

    sidebar and board switch icon

--- js/postile/view/board_more_pop.js
+++ js/postile/view/board_more_pop.js
@@ -4,19 +4,15 @@ goog.require('postile.view');
 goog.require('goog.dom');
 goog.require('goog.events');
 
-postile.view.board_more_pop.BoardMorePop = function() {
+postile.view.board_more_pop.BoardMorePop = function(input_instance) {
     postile.view.NormalView.call(this);
     postile.ui.load(this.container, postile.staticResource(['board_more_pop.html']));
     this.container.id = 'board_more';
-    var button = this.container.children[1];
-    goog.events.listen(button, goog.events.EventType.CLICK, function(){
-        (new postile.view.board_more_pop.OtherBoard()).open(button);
-    });
+
     goog.dom.appendChild(document.body, this.container);
 }
 
 goog.inherits(postile.view.board_more_pop.BoardMorePop, postile.view.NormalView);
-
 postile.view.board_more_pop.BoardMorePop.prototype.unloaded_stylesheets = ['board_more_pop.css'];
 
 postile.view.board_more_pop.OtherBoard = function(input_instance) {
