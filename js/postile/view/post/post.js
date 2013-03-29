9fa8111b28689d6411cb479659457d2298c6e619
guanlun <guanlunzhao@gmail.com>
2013-03-29 20:44:13 +0800

    commenting in expanded post implemented

--- js/postile/view/post/post.js
+++ js/postile/view/post/post.js
@@ -45,41 +45,56 @@ postile.view.post.PostExpand.prototype.unloaded_stylesheets = ['_post_expand.css
 
 postile.view.post.PostExpand.prototype.initComments = function() {
     this.commentBox_el = postile.dom.getDescendantByClass(this.post_el, 'comment-box');
-    this.commentProfileImg = postile.dom.getDescendantByClass(this.commentBox_el, 'img');
+    this.commentProfileImg_el = postile.dom.getDescendantByClass(this.commentBox_el, 'img');
+
+    // comment input area
+    this.commentArea_el = postile.dom.getDescendantByClass(this.commentBox_el, 'input');
+    postile.ui.makeLabeledInput(this.commentArea_el, 'Enter your comments here', 
+            'half_opaque', function() {
+                postile.ajax(
+                        [ 'inline_comment', 'new' ], 
+                        { 
+                            post_id: this.postData.id,
+                            content: this.commentArea_el.innerHTML
+                        }, function(data) {
+                            this.renderComment(data.message.inline_comment);
+                        }.bind(this));
+            }.bind(this));
 
     postile.data_manager.getUserData(localStorage.postile_user_id, function(data) {
-        this.commentProfileImg.src = postile.uploadsResource([ data.image_small_url ]);
+        this.commentProfileImg_el.src = postile.uploadsResource([ data.image_small_url ]);
     }.bind(this));
 
     postile.ajax([ 'inline_comment', 'get_inline_comments' ], { post_id: this.postData.id }, function(data) {
         this.comments = data.message.inline_comments;
-        console.log(data.message);
 
         for (var i in this.comments) {
-            var cmt = this.comments[i].inline_comment;
-
-            postile.data_manager.getUserData(cmt.creator_id, function(userData) {
-                var comment_el = goog.dom.createDom('div', 'comment');
-                goog.dom.appendChild(this.commentContainer_el, comment_el);
-
-                var profileImageContainer_el = goog.dom.createDom('div', 'profile-image');
-                goog.dom.appendChild(comment_el, profileImageContainer_el);
-
-                var profileImage_el = goog.dom.createDom('img', 'img');
-                goog.dom.appendChild(profileImageContainer_el, profileImage_el);
-                profileImage_el.src = postile.uploadsResource([ userData.image_small_url ]);
-
-                var commentRight_el = goog.dom.createDom('div', 'comment-right');
-                goog.dom.appendChild(comment_el, commentRight_el);
-
-                var username_el = goog.dom.createDom('div', 'username');
-                goog.dom.appendChild(commentRight_el, username_el);
-                username_el.innerHTML = userData.username;
-
-                var content_el = goog.dom.createDom('div', 'content');
-                goog.dom.appendChild(commentRight_el, content_el);
-                content_el.innerHTML = cmt.content;
-            }.bind(this));
+            this.renderComment(this.comments[i].inline_comment);
         }
+    }.bind(this));
+}
+
+postile.view.post.PostExpand.prototype.renderComment = function(cmt) {
+    postile.data_manager.getUserData(cmt.creator_id, function(userData) {
+        var comment_el = goog.dom.createDom('div', 'comment');
+        goog.dom.appendChild(this.commentContainer_el, comment_el);
+
+        var profileImageContainer_el = goog.dom.createDom('div', 'profile-image');
+        goog.dom.appendChild(comment_el, profileImageContainer_el);
+
+        var profileImage_el = goog.dom.createDom('img', 'img');
+        goog.dom.appendChild(profileImageContainer_el, profileImage_el);
+        profileImage_el.src = postile.uploadsResource([ userData.image_small_url ]);
+
+        var commentRight_el = goog.dom.createDom('div', 'comment-right');
+        goog.dom.appendChild(comment_el, commentRight_el);
+
+        var username_el = goog.dom.createDom('div', 'username');
+        goog.dom.appendChild(commentRight_el, username_el);
+        username_el.innerHTML = userData.username;
+
+        var content_el = goog.dom.createDom('div', 'content');
+        goog.dom.appendChild(commentRight_el, content_el);
+        content_el.innerHTML = cmt.content;
     }.bind(this));
 }
