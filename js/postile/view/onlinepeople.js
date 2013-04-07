a17f1d961fd7471affaa056c6bb123367f0ce667
Gary LI <litao912002@gmail.com>
2013-04-07 12:32:23 -0500

    Framework fro online people

--- js/postile/view/onlinepeople.js
+++ js/postile/view/onlinepeople.js
@@ -5,8 +5,8 @@ goog.require('postile.view');
 
 postile.view.onlinepeople.OnlinePeople = function(header) {
     //inherits TipView
-    /*
     postile.view.TipView.call(this);
+    /*
     postile.ui.load(this.container,
             postile.conf.staticResource(['_onlinepeople.html']));
     this.container.style.top = '0px';
