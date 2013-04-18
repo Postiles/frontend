6878b53279bb7154d4eeccb12c4e06e5e0735215
kmxz <kxzkxz7139@gmail.com>
2013-04-18 23:12:53 +0800

    kbd to choose from @ persons
    Still with bug, cannot be used for inline cmments

--- js/postile/view/at.js
+++ js/postile/view/at.js
@@ -13,11 +13,17 @@ postile.view.At = function(el) {
     var instance = this;
     this.ipel = el;
     this.container.className = 'at_box';
+    this.current_person = null;
     this.container.innerHTML = '<div class="at-hint">Enter a user name to @</div>';
     this.editlsnr = new postile.events.ContentChangeListener(el, function(e){ instance.realPress(e); });
 
     goog.events.listen(el, goog.events.EventType.KEYUP, instance.keyUpHandler.bind(this));
     goog.events.listen(el, goog.events.EventType.KEYDOWN, instance.keyDownHandler.bind(this));
+    
+    this.keyboard_event_handler = new postile.events.EventHandler(postile.conf.getGlobalKeyHandler(),
+            goog.events.KeyHandler.EventType.KEY, function(e) {
+                instance.keypress(e);
+            });
 }
 
 goog.inherits(postile.view.At, postile.view.TipView);
@@ -69,60 +75,82 @@ postile.view.At.prototype.renderUser = function(profile) {
     goog.dom.appendChild(tmpDiv, avaDiv);
     goog.dom.appendChild(tmpDiv, usnDiv);
     goog.dom.appendChild(tmpDiv, goog.dom.createDom('div', 'clear'));
-
+    tmpDiv.assoc_profile = profile;
+    
     goog.events.listen(tmpDiv, goog.events.EventType.CLICK, function() {
-        var atNode = goog.dom.createDom('span', 'at_tag');
-        atNode.contentEditable = true;
-        atNode.innerHTML = '@' + profile.username
+        instance.choose(this.assoc_profile);
+    });
 
-        atNode.setAttribute('at-user-name', profile.username);
-        atNode.setAttribute('at-user', profile.user_id);
-        
-        instance.range.deleteContents();
-
-        instance.range.insertNode(atNode);
-        instance.range.collapse();
-        
-        instance.range.insertNode(document.createTextNode('\u00a0'));
-        instance.range.collapse();
-
-        var sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(instance.range);
-        
-        instance.close();
-        
-        if (!instance.ipel._at_consist_listener) {
-            instance.ipel._at_consist_listener = new postile.events.ContentChangeListener(instance.ipel, function() {
-                var ats = postile.dom.getDescendantsByCondition(instance.ipel, function(cel) {
-                    return cel.tagName && cel.getAttribute('at-user');
-                });
-                var corr;
-                for (var i in ats) {
-                    corr = '@' + ats[i].getAttribute('at-user-name');
-                    if (ats[i].innerHTML.length < corr.length) {
-                        ats[i].fontStyle = 'normal';
-                        goog.dom.removeNode(ats[i]);
-                    } else if (ats[i].innerHTML.length > corr.length) {
-                        ats[i].innerHTML = corr;
-                        (function() {
-                            var newNode = goog.dom.createTextNode(" ");
-                            goog.dom.insertSiblingAfter(newNode, ats[i]);
-                            var range = document.createRange();
-                            range.selectNodeContents(newNode);
-                            var sel = window.getSelection();
-                            sel.removeAllRanges();
-                            sel.addRange(range);
-                        })();
-                    } else {
-                        ats[i].innerHTML = corr;
-                    }
-                }
-            });
-            instance.ipel._at_consist_listener.listen();
-        }
+    goog.events.listen(tmpDiv, goog.events.EventType.MOUSEOVER, function() {
+        instance.setCurrent(this);
+    });
+    goog.events.listen(tmpDiv, goog.events.EventType.MOUSEOUT, function() {
+        instance.setCurrent(this);
     });
     return tmpDiv;
+}
+
+postile.view.At.prototype.choose = function(profile) {
+    var instance = this;
+    var atNode = goog.dom.createDom('span', 'at_tag');
+    atNode.contentEditable = true;
+    atNode.innerHTML = '@' + profile.username
+
+    atNode.setAttribute('at-user-name', profile.username);
+    atNode.setAttribute('at-user', profile.user_id);
+    
+    instance.range.deleteContents();
+
+    instance.range.insertNode(atNode);
+    instance.range.collapse();
+    
+    instance.range.insertNode(document.createTextNode('\u00a0'));
+    instance.range.collapse();
+
+    var sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(instance.range);
+    
+    instance.close();
+    
+    if (!instance.ipel._at_consist_listener) {
+        instance.ipel._at_consist_listener = new postile.events.ContentChangeListener(instance.ipel, function() {
+            var ats = postile.dom.getDescendantsByCondition(instance.ipel, function(cel) {
+                return cel.tagName && cel.getAttribute('at-user');
+            });
+            var corr;
+            for (var i in ats) {
+                corr = '@' + ats[i].getAttribute('at-user-name');
+                if (ats[i].innerHTML.length < corr.length) {
+                    ats[i].fontStyle = 'normal';
+                    goog.dom.removeNode(ats[i]);
+                } else if (ats[i].innerHTML.length > corr.length) {
+                    ats[i].innerHTML = corr;
+                    (function() {
+                        var newNode = goog.dom.createTextNode(" ");
+                        goog.dom.insertSiblingAfter(newNode, ats[i]);
+                        var range = document.createRange();
+                        range.selectNodeContents(newNode);
+                        var sel = window.getSelection();
+                        sel.removeAllRanges();
+                        sel.addRange(range);
+                    })();
+                } else {
+                    ats[i].innerHTML = corr;
+                }
+            }
+        });
+        instance.ipel._at_consist_listener.listen();
+    }
+}
+
+postile.view.At.prototype.setCurrent = function(current) {
+    if (!current) { return; }
+    if (this.current_person) {
+        goog.dom.classes.remove(this.current_person, 'hover');
+    }
+    goog.dom.classes.add(current, 'hover');
+    this.current_person = current;
 }
 
 postile.view.At.prototype.fetchAndRender = function(keyword) {
@@ -134,7 +162,28 @@ postile.view.At.prototype.fetchAndRender = function(keyword) {
         for (var i in usr) {
             goog.dom.appendChild(instance.container, instance.renderUser(usr[i].profile));
         }
+        if (usr.length) {
+            instance.setCurrent(instance.container.firstChild);
+        } else {
+            instance.current_person = null;
+        }
     });
+}
+
+postile.view.At.prototype.keypress = function(e) {
+    if (!this.current_person) { return; }
+    switch (e.keyCode) {
+        case goog.events.KeyCodes.UP:
+            this.setCurrent(this.current_person.previousSibling);
+            break;
+        case goog.events.KeyCodes.DOWN:
+            this.setCurrent(this.current_person.nextSibling);
+            break;
+        case goog.events.KeyCodes.ENTER:
+            this.choose(this.current_person.assoc_profile);
+            e.stopPropagation();
+            e.preventDefault();
+    }
 }
 
 postile.view.At.prototype.open = function() {
@@ -147,10 +196,12 @@ postile.view.At.prototype.open = function() {
     this.range = this.oRange.cloneRange();
     postile.view.TipView.prototype.open.call(this, this.ipel);
     this.editlsnr.listen();
+    this.keyboard_event_handler.listen();
 }
 
 postile.view.At.prototype.close = function() {
     this.editlsnr.unlisten();
+    this.keyboard_event_handler.unlisten();
     postile.view.TipView.prototype.close.call(this);
 }
 
