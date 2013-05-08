a8bee1e8c062899f0976aa43a39a2740f98824a2
Gary LI <litao912002@gmail.com>
2013-05-08 18:02:12 -0500

    Tracking ID changed

--- js/postile/utils/gapostile.js
+++ js/postile/utils/gapostile.js
@@ -4,7 +4,8 @@ goog.provide('postile.analysis');
 postile.analysis._gaq = null;
 postile.analysis.init = function() {
     postile.analysis._gaq = postile.analysis._gaq||[];
-    postile.analysis._gaq.push(['_setAccount', 'UA-40739447-1']);
+    postile.analysis._gaq.push(['_setAccount', 'UA-40741449-1']);
+    postile.analysis._gaq.push(['_trackPageview']);
     (function() {
        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
